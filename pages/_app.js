e691a65ddeaa349551079a733ef4a90d11d40bcc
RooshiPhakey <r_phakey@hotmail.com>
2025-08-10 18:12:41 +0100

    Add files via upload

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,2 @@
-// pages/_app.js
 import '../styles/globals.css'
-import AntiAutoScroll from '../components/AntiAutoScroll'
-
-export default function App({ Component, pageProps }) {
-  return (
-    <>
-      <AntiAutoScroll />
-      <Component {...pageProps} />
-    </>
-  )
-}
+export default function App({ Component, pageProps }) { return <Component {...pageProps} /> }
